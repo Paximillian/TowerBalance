e11402445ec7c3848f9a3fdb9426aa08225fd1fd
Paximillian <morzil@walla.com>
2015-10-11 17:01:41 +0300

    Added brick sound

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Block : MonoBehaviour
+{
+    private AudioSource m_BrickHitSound;
+
+    // Use this for initialization
+    void Start()
+    {
+        m_BrickHitSound = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter2D(Collision2D i_Collision)
+    {
+        m_BrickHitSound.Play();
+    }
+}
